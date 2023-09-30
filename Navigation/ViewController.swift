0f876b0142e8e5ae88f225342520ca9b7cf3a88c
Irina Bashkova <iramoo@bk.ru>
2023-09-30 20:38:20 +0300

    новый комит

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -2,18 +2,61 @@
 //  ViewController.swift
 //  Navigation
 //
-//  Created by Павел Пушкин on 02.07.2023.
+//  Created by Irina Bashkova
 //
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    private lazy var actionButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Перейти", for: .normal)
+        
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(actionButton)
+        
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            actionButton.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor,
+                constant: 20.0
+            ),
+            actionButton.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor,
+                constant: -20.0
+            ),
+            actionButton.centerYAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.centerYAnchor),
+            actionButton.heightAnchor.constraint(equalToConstant: 44.0)
+        ])
+        
+        actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
     }
-
-
+    
+    @objc func buttonPressed(_ sender: UIButton) {
+        let profileViewController = FeedViewController()
+        
+        profileViewController.modalTransitionStyle = .coverVertical
+        profileViewController.modalPresentationStyle = .fullScreen
+        
+        present(profileViewController, animated: true)
+    }
 }
+
+
+//    override func viewWillAppear(_ animated: Bool) {
+//          super.viewWillAppear(animated)
+//        }
+
+//      }
+
+// тест 2
+// тест 3
 
